6be2e5361270ad120fd9d515851fd21a6fc210bf
RuzilKutliakhmetov <115926681+RuzilKutliakhmetov@users.noreply.github.com>
2024-03-10 14:37:16 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ButtonComponent from './components/ButtonComponent';
+import queryString from "query-string"
 
-function App() {
+const App = () => {
+  const [authorizationCode, setAuthorizationCode] = useState('');
+  const [accessToken, setAccessToken] = useState('');
+
+  const queryParams = queryString.parse(window.location.search)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <script
-        className="amocrm_oauth"
-        data-client-id="b4415501-f3d5-45b7-992c-4ff08f29b5f8"
-        data-title="Button"
-        data-compact="false"
-        data-class-name="className"
-        data-color="default"
-        data-state="state"
-        data-error-callback="functionName"
-        data-mode="popup"
-        src="https://www.amocrm.ru/auth/button.min.js"
-      ></script>
+    <div>
+      <h1>Authorization Code: {authorizationCode}</h1>
+      <h2>Access Token: {accessToken}</h2>
+      <p>Value of term: {queryParams.term}</p>
+      <p>
+        All query params <pre>{JSON.stringify(queryParams, null, 2)}</pre>
+      </p>
+      <ButtonComponent />
     </div>
   );
-}
+};
 
 export default App;
